cb44bb97c5b1a3607e2d7543f72db8841fa40b8f
ray-x <rayx.cn@gmail.com>
2022-06-09 20:07:22 +1000

    trim result

--- lua/cmp_treesitter/init.lua
+++ lua/cmp_treesitter/init.lua
@@ -3,14 +3,14 @@ local treesitter = require('cmp_treesitter.treesitter')
 local defaults = {
   keyword_pattern = [[\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%([\-]\w*\)*\)]],
   get_bufnrs = function()
-    return {vim.api.nvim_get_current_buf()}
-  end
+    return { vim.api.nvim_get_current_buf() }
+  end,
 }
 
 local source = {}
 
 source.new = function()
-  local self = setmetatable({}, {__index = source})
+  local self = setmetatable({}, { __index = source })
   self.treesitters = {}
   return self
 end
@@ -19,20 +19,29 @@ source.get_keyword_pattern = function(_, params)
   params.option = vim.tbl_deep_extend('keep', params.option, defaults)
   vim.validate({
     keyword_pattern = {
-      params.option.keyword_pattern, 'string', '`opts.keyword_pattern` must be `string`'
+      params.option.keyword_pattern,
+      'string',
+      '`opts.keyword_pattern` must be `string`',
     },
-    get_bufnrs = {params.option.get_bufnrs, 'function', '`opts.get_bufnrs` must be `function`'}
+    get_bufnrs = { params.option.get_bufnrs, 'function', '`opts.get_bufnrs` must be `function`' },
   })
   return params.option.keyword_pattern
+end
+
+local function trim(s)
+  s = s:match('^%s*(.-)%s*$')
+  return s:match('^%p*(.-)%p*$')
 end
 
 source.complete = function(self, params, callback)
   params.option = vim.tbl_deep_extend('keep', params.option, defaults)
   vim.validate({
     keyword_pattern = {
-      params.option.keyword_pattern, 'string', '`opts.keyword_pattern` must be `string`'
+      params.option.keyword_pattern,
+      'string',
+      '`opts.keyword_pattern` must be `string`',
     },
-    get_bufnrs = {params.option.get_bufnrs, 'function', '`opts.get_bufnrs` must be `function`'}
+    get_bufnrs = { params.option.get_bufnrs, 'function', '`opts.get_bufnrs` must be `function`' },
   })
 
   local processing = true
@@ -49,20 +58,20 @@ source.complete = function(self, params, callback)
     -- print('f', flg)
     if flg and tsnds.words ~= nil then
       for _, word in ipairs(tsnds.words) do
-        if not words[word.word] and input ~= word then
-          words[word.word] = true
-          local w = word.word
+        local w = trim(word.word or '')
+        if not words[w] and input ~= word then
+          words[w] = true
           if #w > 25 then
             w = string.sub(w, 1, 25) .. ''
           end
-          table.insert(items, {label = w, insertText = word.word, dup = 0})
+          table.insert(items, { label = w, insertText = word.word, dup = 0 })
         end
       end
     end
   end
   processing = true
   -- print("items", vim.inspect(items))
-  callback({items = items, isIncomplete = processing})
+  callback({ items = items, isIncomplete = processing })
 end
 
 --- _get_treesitters
